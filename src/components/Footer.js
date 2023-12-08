ad668b065bd920afd0de5dc033d7b9f60864d169
Nick Maxwell <nickmaxwell10@gmail.com>
2023-12-07 21:19:06 -0800

    Footer styling for desktop & mobile

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,65 +1,61 @@
-
 import Link from "next/link";
+import { FaFacebook, FaEnvelope, FaShareAlt } from "react-icons/fa";
 
-
-const Footer = ({site, pages}) => {
+const Footer = ({ site, pages }) => {
   // console.log(site)
   return (
-<div className='flex flex-col'>
-    <footer className="footer p-10 bg-secondary text-secondary-content">
-     <aside>
-        <h1 className='text-3xl'>{site.name}</h1>
-        <h4 className='text-md'>{site.description}</h4>
-        
+    <div className="flex flex-col">
+      <footer className="min-h-[60vh] footer p-10 bg-neutral text-neutral-content">
+        <aside>
+          <h1 className="text-2xl font-thin">{site.name}</h1>
+          <h4 className="flex flex-row space-x-4 mt-5">
+            <FaFacebook size={"20px"} /> <FaEnvelope size={"20px"} />{" "}
+            <FaShareAlt size={"20px"} />
+          </h4>
         </aside>
-     
-  
-    <nav>
-      <header className="footer-title space-y-2">Content</header> 
-    {pages.map((page) => (
-          <li key={page.id}>
-          <Link className="link link-hover" href={page.slug}>{page.title}</Link>
-          {/*<p>{page.page_description}</p>*/}
-          </li>
-        ))}
-    
-    </nav>
- <nav>
- <header className="footer-title space-y-2">{site.name}</header> 
-    <li key="search">
-           <Link href={'/search'}>Search</Link>
-           </li>
 
-         { site.show_contact_form &&  <li key={'contact'}>
-             <Link href="/contact">Contact</Link>
-         </li> }
-           <li key="sitemap">
-        <Link href={'/sitemap.xml'}>Sitemap</Link>
-        </li>
-         </nav>
-  </footer> 
+        <nav>
+          <p className="p-2 pl-0 text-sm font-thin">
+            725 Broad St <br /> Los Angeles, CA 90291
+          </p>
+          <p className="p-2 pl-0 text-sm font-thin">
+            info@busiconsu.com <br />
+            (310) 384-9902
+          </p>
+        </nav>
+        <nav>
+          <Link className="underline" href={"/about"}>About</Link>
+          <Link className="underline pt-3" href="/services">Services</Link>
+          <Link className="underline pt-3" href={"/contact"}>Contact</Link>
+        </nav>
+      </footer>
 
-
-  { site.ad_level === "no_ads" || site.ad_level === "no_ads_with_amazon" ? null :
-<div  className='p-12 self-center  '>
-  
-        <div data-theme='pagesake' className='card max-w-2xl p-4 bg-black'>
-          <figure><img className='w-64 p-4' src="/pagesake_logo.png" /></figure>
-          <div className='card-body'>
-            <h4 className='card-title'>Built With Pagesake </h4>
-            <p>Create revenue-generating websites for free, in seconds, with Pagesake. What could you do with a free website?</p>
+      {site.ad_level === "no_ads" ||
+      site.ad_level === "no_ads_with_amazon" ? null : (
+        <div className="p-12 self-center  ">
+          <div data-theme="pagesake" className="card max-w-2xl p-4 bg-black">
+            <figure>
+              <img className="w-64 p-4" src="/pagesake_logo.png" />
+            </figure>
+            <div className="card-body">
+              <h4 className="card-title">Built With Pagesake </h4>
+              <p>
+                Create revenue-generating websites for free, in seconds, with
+                Pagesake. What could you do with a free website?
+              </p>
             </div>
-            <div className='card-actions justify-center'>
-            <Link href='https://pagesake.com/how-it-works' className='btn'>Learn More</Link>
-            <Link href='https://pagesake.com' className='btn btn-primary'>Start Free</Link>
+            <div className="card-actions justify-center">
+              <Link href="https://pagesake.com/how-it-works" className="btn">
+                Learn More
+              </Link>
+              <Link href="https://pagesake.com" className="btn btn-primary">
+                Start Free
+              </Link>
             </div>
+          </div>
         </div>
-   </div> 
-
- }
-
-
-        </div>
+      )}
+    </div>
   );
 };
 export default Footer;
