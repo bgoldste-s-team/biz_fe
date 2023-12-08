9c99e4a45786910b0d55a293927bd02348c500fb
Nick Maxwell <nickmaxwell10@gmail.com>
2023-12-07 20:50:37 -0800

    Mobile styling for TextBlock - No image URL provided

--- src/components/TextBlock.js
+++ src/components/TextBlock.js
@@ -1,8 +1,6 @@
 import React from "react";
-import Link from "next/link";
 import ReactMarkdown from "react-markdown";
 import remarkGfm from "remark-gfm";
-import { useState } from "react";
 
 const TextBlock = ({
   key,
@@ -17,32 +15,36 @@ const TextBlock = ({
   image_link,
   index,
 }) => {
-  const [showFullText, setShowFullText] = useState(false);
   return (
     <div
       key={key}
       contentBlockId={contentBlockId}
-      className={`bg-neutral sm:p-24 sm:py-12 sm:pt-24 rounded-none rounded-t-2xl card md:card-side ${
+      className={`bg-neutral sm:p-24 lg:pr-0 lg: mr-0 sm:py-12 sm:pt-0 rounded-none rounded-t-2xl card md:card-side items-end ${
         index % 2 === 0 ? "md:text-right" : "md:flex-row-reverse "
       }   `}
     >
-      <img
+      {image_link ? (
+        <img
           src={image_link}
           alt="Hero Image"
-          className="sm:object-fill rounded-t-2xl sm:rounded-2xl h-64 sm:w-full"
+          className="sm:mt-24 sm:mr-12 sm:ml-12 sm:object-fill rounded-t-2xl sm:rounded-2xl h-64 w-full"
         />
+      ) : (
+        <img
+          src={"/textblock_bg.svg"}
+          alt="Hero Image"
+          className="h-3/6 relative top-0 float-right rounded-t-2xl sm:rounded-2xl sm:w-4/6"
+        />
+      )}
 
-      <div className="flex flex-col text-neutral-content p-12 sm:p-0 sm:mr-20">
+      <div className="flex flex-col text-neutral-content p-12 sm:p-0 sm:mr-2 sm:pt-24">
         {subheader && <h3 className="text-lg uppercase">{subheader}</h3>}
         <h2 className="text-4xl font-thin">{header}</h2>
 
         <div className="mt-5 font-thin">
           <ReactMarkdown plugins={[remarkGfm]} className="">
-            {showFullText ? body : body.slice(0, 200) + "..."}
+            {body.slice(0, 2000)}
           </ReactMarkdown>
-          <div className="link" onClick={() => setShowFullText(!showFullText)}>
-            {showFullText ? "see less" : "see more"}
-          </div>
         </div>
         {/* <div className='btn btn-primary'>Subscribe</div> */}
       </div>
