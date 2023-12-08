59a5f7f2cf079d5b0e6c8d8952b9ec5e24d04665
Nick Maxwell <nickmaxwell10@gmail.com>
2023-12-07 17:13:09 -0800

    Updated navbar to pure-tailwindcss and support for desktop & mobile

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,107 +1,88 @@
-
-// import Link from "next/link";
-// import {FaBars} from "react-icons/fa6";
-
-// const Navbar = ({site}) => {
-
-
-//   // const { siteName, siteImgUrl }  = useContext(AppContext);
-//   const siteName = site.name
-
-//     console.log(site)
-//     console.log(site.pages)
-//     const pages = site.pages;
-//     console.log(pages)
-//     const determineFontSize = (text) => {
-//       const length = text.length;
-
-//       if (length < 10) return 'text-2xl';
-//       if (length < 20) return 'text-xl';
-//       if (length < 30) return 'text-base';
-//       return 'text-md';
-//   };
-//     const dynamicFontSize = determineFontSize(siteName);
-//   return (
-
-
-
-//       <div className="navbar bg-base-100 z-50 top-0 sticky">
-
-//   <div className="navbar-start ">
-//     <div className='break-words py-2'>
-//     <Link href="/" className={`text-left btn btn-ghost normal-case ${dynamicFontSize}  `}>
-//     {siteName}</Link>
-//     </div>
-//   </div>
-//   <div className="navbar-end">
-//     <div className="dropdown dropdown-end">
-//       <label tabIndex={0} className="btn btn-primary btn-outline">
-//         <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 text-base-content" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h7" /></svg>
-//       </label>
-//       <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
-
-//                       <li key={'home'}>
-//                           <Link href="/">Home</Link>
-//                       </li>
-//                       {pages.filter(p => p.show_in_navbar === true && !p.is_homepage).map( (p) => (
-//                           <li key={p.id}>
-//                               <Link href={p.slug}>{p.title}</Link>
-//                           </li>
-//                       ))}
-//                         <li key={'search'}>
-//                           <Link href="/search">Search</Link>
-//                       </li>
-
-
-
-       
-//       </ul>
-//     </div>
-//   </div>
-  
-// </div>
-
-
-//   )
-
-
-// };
-
-// export default Navbar;
 import Link from "next/link";
+import { useState } from "react";
 
 const Navbar = ({ site }) => {
-    const siteName = site.name;
-     const pages = site.pages;
-        const determineFontSize = (text) => {
-        const length = text.length;
+  const [isOpen, setIsOpen] = useState(false);
+  const siteName = site.name;
+  const pages = site.pages;
+  const determineFontSize = (text) => {
+    const length = text.length;
 
-        if (length < 10) return 'text-2xl leading-snug';
-        if (length < 20) return 'text-xl leading-snug';
-        if (length < 30) return 'text-base leading-snug';
-        return 'text-md leading-snug';
-    };
-    const dynamicFontSize = determineFontSize(siteName);
+    if (length < 10) return "text-2xl leading-snug";
+    if (length < 20) return "text-xl leading-snug";
+    if (length < 30) return "text-base leading-snug";
+    return "text-md leading-snug";
+  };
+  const dynamicFontSize = determineFontSize(siteName);
 
-    return (
-     <div className="navbar bg-base-100 z-50 top-0 sticky flex items-center">
-            <div className="navbar-start sm:pb-0 ml-24">
-                <div className=" break-words py-4 ">
-                    <Link href="/"className={`text-left btn btn-ghost pl-0 normal-case   ${dynamicFontSize}`}>
-                            {siteName}
-                     
-                    </Link>
-                </div>
-            </div>
-            <div className="navbar-end">
-             
-   
-            
-          </div>
+  return (
+    <header className="sm:flex sm:justify-between sm:items-center sm:pr-4 sm:py-3">
+      <div className="flex items-center justify-between px-4 py-3 sm:p-0">
+        <div>
+          <Link
+            href="/"
+            className={`sm:ml-20 font-medium text-left btn btn-ghost normal-case   ${dynamicFontSize}`}
+          >
+            {siteName}
+          </Link>
         </div>
-    )
+        <div className="sm:hidden">
+          <button
+            type="button"
+            className="block focus:outline-none"
+            onClick={() => {
+              setIsOpen(!isOpen);
+            }}
+          >
+            <svg className="h-6 w-6 fill-current" viewBox="0 0 24 24">
+              {isOpen ? (
+                <path
+                  fillRule="evenodd"
+                  d="M18.278 16.864a1 1 0 0 1-1.414 1.414l-4.829-4.828-4.828 4.828a1 1 0 0 1-1.414-1.414l4.828-4.829-4.828-4.828a1 1 0 0 1 1.414-1.414l4.829 4.828 4.828-4.828a1 1 0 1 1 1.414 1.414l-4.828 4.829 4.828 4.828z"
+                />
+              ) : (
+                <path
+                  fillRule="evenodd"
+                  d="M4 5h16a1 1 0 0 1 0 2H4a1 1 0 1 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2z"
+                />
+              )}
+              ;
+            </svg>
+          </button>
+        </div>
+      </div>
+      <nav
+        className={`${
+          isOpen ? "block" : "hidden"
+        } px-2 pt-2 pb-4 sm:flex sm:p-0 text-center`}
+      >
+        <Link
+          href="/"
+          className={`text-lg block px-2 py-2 rounded hover:opacity-50`}
+        >
+          About
+        </Link>
+        <Link
+          href="/"
+          className={`sm:ml-10 text-lg block px-2 py-2 rounded hover:opacity-50`}
+        >
+          Services
+        </Link>
+        <Link
+          href="/"
+          className={`sm:ml-10 text-lg block px-2 py-2 rounded hover:opacity-50`}
+        >
+          Team
+        </Link>
+        <Link
+          href="/"
+          className={`sm:ml-10 text-lg block px-2 py-2 rounded hover:opacity-50`}
+        >
+          Contact
+        </Link>
+      </nav>
+    </header>
+  );
 };
 
 export default Navbar;
-
-
