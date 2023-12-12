466efbd174ac9b5c98fcb5da728669fcc56e3c79
b <bengoldstein9@gmail.com>
2023-12-12 11:58:11 -0800

    updates, prose

--- src/components/TextBlock.js
+++ src/components/TextBlock.js
@@ -19,7 +19,7 @@ const TextBlock = ({
     <div
       key={key}
       contentBlockId={contentBlockId}
-      className={`bg-neutral sm:p-24 lg:pr-0 lg: mr-0 sm:py-12 sm:pt-0 rounded-none rounded-t-2xl card md:card-side items-end ${
+      className={`bg-neutral lg:p-24 lg:pr-0 lg: mr-0 lg:py-12 lg:pt-0 rounded-none rounded-t-2xl card md:card-side items-end ${
         index % 2 === 0 ? "md:text-right" : "md:flex-row-reverse "
       }   `}
     >
@@ -27,22 +27,21 @@ const TextBlock = ({
         <img
           src={image_link}
           alt="Hero Image"
-          className="sm:mt-24 sm:mr-12 sm:ml-12 sm:object-fill rounded-t-2xl sm:rounded-2xl h-64 w-full"
+          className="lg:mt-24 lg:mr-12 lg:ml-12 lg:object-fill rounded-t-2xl lg:rounded-2xl h-64 w-full"
         />
       ) : (
         <img
           src={"/textblock_bg.svg"}
           alt="Hero Image"
-          className="h-3/6 relative top-0 float-right rounded-t-2xl sm:rounded-2xl sm:w-4/6"
+          className="h-3/6 relative top-0 float-right rounded-t-2xl lg:rounded-2xl lg:w-4/6"
         />
       )}
 
-      <div className="flex flex-col text-neutral-content p-12 sm:p-0 sm:mr-2 sm:pt-24">
-        {subheader && <h3 className="text-lg uppercase">{subheader}</h3>}
+      <div className="flex flex-col text-neutral-content p-12 lg:p-0 lg:mr-2 lg:pt-24">
         <h2 className="text-4xl font-thin">{header}</h2>
 
         <div className="mt-5 font-thin">
-          <ReactMarkdown plugins={[remarkGfm]} className="">
+          <ReactMarkdown plugins={[remarkGfm]} className="prose text-neutral-content">
             {body.slice(0, 2000)}
           </ReactMarkdown>
         </div>
